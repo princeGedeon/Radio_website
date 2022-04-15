5793818bb7d019faf089ee1bd3177bba6b94451a
princeGedeon <guedjegedeon03@gmail.com>
2022-04-15 17:07:15 +0100

    Systeme de reinstialisation de mot de passe fonctionnelle

--- src/website_radio/settings.py
+++ src/website_radio/settings.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
 from pathlib import Path
-
+#COnfiguration SMTP
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS=True
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_HOST_USER="devprincegedeon@gmail.com"
+EMAIL_HOST_PASSWORD="sgtbmcsqxwsrwqbd"
+EMAIL_PORT=587
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
