8e86b2a3760c20fa5636cbaea1afb5d6ed169fd6
Prince Gedeon GUEDJE <86509263+princeGedeon@users.noreply.github.com>
2022-04-15 19:26:45 +0100

    Update settings.py

--- src/website_radio/settings.py
+++ src/website_radio/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS=True
 EMAIL_HOST="smtp.gmail.com"
-EMAIL_HOST_USER="devprincegedeon@gmail.com"
-EMAIL_HOST_PASSWORD="sgtbmcsqxwsrwqbd"
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
 EMAIL_PORT=587
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
